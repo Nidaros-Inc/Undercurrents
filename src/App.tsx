c20234f12704d53967625e60dd99f29e48057990
Nidaros-Inc <jjamesronaldson@rocketmail.com>
2026-01-19 21:21:11 +0000

    Update background gradient colors in App component

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
 };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-[#081428] via-[#081428] to-[#110f2d] text-slate-300 p-8">
+    <div className="min-h-screen bg-gradient-to-br from-[#0a1830] via-[#0a1830]  to-[#18135a] -slate-300 p-8">
       <div className="max-w-4xl mx-auto">
         <header className="text-center mb-14">
  <h1 className="text-6xl font-extrabold tracking-tight mb-4 text-white">
