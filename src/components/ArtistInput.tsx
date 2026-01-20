f33d6e1a6f90e61a190919114216453063674cc1
Nidaros-Inc <jjamesronaldson@rocketmail.com>
2026-01-20 17:33:47 +0000

    Update ArtistInput.tsx

--- src/components/ArtistInput.tsx
+++ src/components/ArtistInput.tsx
@@ -43,7 +43,7 @@ export default function ArtistInput({ artists, onAdd, onRemove, maxArtists }: Pr
         placeholder:text-slate-500
         pl-4
         transition-all duration-200
-        focus:outline-none focus:ring-2 focus:ring-slate-250 focus:ring-opacity-40
+        focus:outline-none focus:ring-2 focus:ring-slate-300 focus:ring-opacity-40
       "
     />
 
